1cd8f30158e430791acb3ca8170ed38214af1d19
kodaman2 <ic3balandran@yahoo.com>
2019-07-03 18:36:57 -0500

    Initial release v1.0.13

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name='Generatrix',
-    version='1.0.0',
+    version='1.0.13',
     author='Fernando A. Balandran',
     author_email='fredz0003@gmail.com',
     description='Generates repo contents markdown',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    license='MIT',
     url='https://github.com/kodaman2/generatrix_py',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     entry_points={
         'console_scripts': [
-            'gtrix = generatrix:main',
+            'gtrix = generatrix_py.generatrix:main',
         ],
-    }
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    
 )
